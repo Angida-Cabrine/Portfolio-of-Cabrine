b70c356dc73f2e62a24c84ff179f0a087b2b2efb
Angida-Cabrine <132916432+Angida-Cabrine@users.noreply.github.com>
2026-04-25 10:44:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("year").textContent = new Date().getFullYear();
 })
 
+function downloadCV(){
+    window.location.href = "Cabrine Assets/ANGIDA CABRINE CV.pdf";
+}
+
  
